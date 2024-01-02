2acd775fce97908ea34885aa95fed5ee91b281c9
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-02 20:55:13 +0400

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable{
+public static void main(String[] arg){
+for (int i=0, a=5, b=8; i<6; i++){
+System.out.println(i);
+System.out.println(a);
+System.out.println(b);
+}
+}
+
+}
